e16b60d3d4800774fb9a8456e0e8fd29b6f6929c
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-06-24 14:49:14 -0700

    switchImage works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-// import logo from './logo.svg';
 import './App.css';
+import {useWindowDimensions} from './functions/getWindowDimensions.js';
+import $ from 'jquery'
+import {imagesArray} from './assets/images/imagesArray.js'
 
 function App() {
+  const { height, width } = useWindowDimensions();
+
+
+
+  const switchImage = () => {
+    console.log('clicked')
+    $('.main-console').css('background-image', `url("${imagesArray[0]}")`)
+  }
+
   return (<>
-    <div className="App">
-      <header className="App-header">
-        <p>React</p>
-      </header>
+    <div className="App-background">
+      <div className="App-container" style={{height: height, width: width}}>>
+        <div onClick={() => switchImage()} className="main-console" >
+      </div>
+      <div>
+      {/* <ImageGallery items={images} /> */}
+      </div>
       <script type="text/babel" src='./textInterface/textInterface.js'></script>
+    </div>
     </div>
   </>
   );
