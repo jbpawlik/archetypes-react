d63e00801dac2ef156a5544d7cc3953ff335429a
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-07 22:44:49 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useWindowDimensions} from './functions/getWindowDimensions.js';
 import $ from 'jquery'
 import {imagesArray} from './assets/images/imagesArray.js'
@@ -10,18 +10,8 @@ function App() {
   const [leftButtonPressed, pressLeftButton] = useState(false)
   const [rightButtonPressed, pressRightButton] = useState(false)
 
-  const switchImage = (num) => {
+  useEffect(() => {
 
-    if (num === 0 && leftButtonPressed === false) {
-      pressLeftButton(true)
-    } else if (num === 0 && leftButtonPressed === true) {
-      pressLeftButton(false)
-    }
-    if (num === 1 && rightButtonPressed === false) {
-      pressRightButton(true)
-    } else if (num === 1 && rightButtonPressed === true) {
-      pressRightButton(false)
-    }
     if (leftButtonPressed === false && rightButtonPressed === false) {
       setSelectedImage(imagesArray[2])
     } else if (leftButtonPressed === true && rightButtonPressed === false) {
@@ -31,19 +21,51 @@ function App() {
     } else if (leftButtonPressed === false && rightButtonPressed === true) {
       setSelectedImage(imagesArray[3])
     }
-    console.log(imageSelected)
+    $('.main-console').css('background-image', `url("${imageSelected}")`)
 
-    
-    console.log(imageSelected)
+    // setSelectedImage(imageSelected)
+
+    return () => {
+
+    }
+  }, [imageSelected, leftButtonPressed, rightButtonPressed])
+  
+
+  const switchImage = (num) => {
+
+    if (num === 0 && leftButtonPressed === false) {
+      pressLeftButton(true)
+      // $('.Left-text').css('display', 'none')
+
+    } else if (num === 0 && leftButtonPressed === true) {
+      pressLeftButton(false)
+      // $('.Left-text').css('display', 'block')
+
+
+    }
+    if (num === 1 && rightButtonPressed === false) {
+      pressRightButton(true)
+      // $('.Right-text').css('display', 'none')
+
+    } else if (num === 1 && rightButtonPressed === true) {
+      pressRightButton(false)
+      // $('.Right-text').css('display', 'block')
+
+
+    }
+
 
   }
-  $('.main-console').css('background-image', `url("${imageSelected}")`)
+
   return (<>
     <div className="App-background">
       <div className="App-container" style={{height: height, width: width}}>>
         <div className="main-console" style={{height: height, width: width}}>>
-        <div><h1 className="App-title">ARCHETYPES</h1></div>
-
+        <div>
+          {/* <h1 className="App-title" onClick={() => $('.App-title').css('display', 'none') }>ARCHETYPES</h1> */}
+          {/* <h2 className="Left-text">ASK</h2>
+          <h2 className="Right-text">ANSWER</h2> */}
+        </div>
         <div style={{display: 'flex'}}>
           <div className='leftSideButton' style={{height: height, width: width/2, left: 0}} onClick={()=>{switchImage(0)}}></div>
           <div className='rightSideButton' style={{height: height, width: width/2, right: 0}} onClick={()=>{switchImage(1)}}>
