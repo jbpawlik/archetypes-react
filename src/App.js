8f84c402335a0faa00574447a472c3bf44770c3c
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-13 15:35:47 -0700

    add CLI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import './App.css';
-import React, {} from 'react'
+import React, {useState } from 'react'
 import SelfSimulator from './components/SelfSimulator.js'
-import {useWindowDimensions} from './functions/getWindowDimensions.js';
+import $ from 'jquery'
+import useSound from 'use-sound'
+import rustle from './assets/sounds/rustle.wav';
+import blanket from './assets/sounds/blanket.wav';
+import sweatpants from './assets/sounds/sweatpants.wav';
+import woodclack from './assets/sounds/woodclack.wav'
+import woodtap from './assets/sounds/woodtap.wav';
+import { playerContext } from './components/playerContext.js';
+import CommandLineTerminal from './components/CommandLineTerminal.js';
+// import {TextInterface} from './TextInterface/TextInterface.js';
+// import {useWindowDimensions} from './functions/getWindowDimensions.js';
 
 function App() {
-  const { height, width } = useWindowDimensions();
+  // const { height, width } = useWindowDimensions();
+  const [intro] = useSound(woodclack)
+  const [titleRemoved, removeTitle] = useState(false)
+  const [playerProfile, setPlayerProfile] = useState({}) 
+
+  // const scriptFile = $.getScript('../src/textInterface/textInterface.js')
+
+  // console.log(scriptFile)
+
+  // document.addEventListener("DOMContentLoaded", function() {
+  //   console.log(TextInterface());
+  // });
+
+  // const script = document.createElement('script');
+  // script.type = 'text/babel';
+  // script.src = '../src/textInterface/textInterface.js';
+  // document.head.appendChild(script);
+
+  // fetch("../src/textInterface/textInterface.js")
+  // .then((response) => console.log(response))
+
+  const clickTitle = () => {
+    if (!titleRemoved) {
+    intro()
+    $('.App-title').remove()
+    removeTitle(true)
+    $('.main-console').css('display', 'flex')
+  } else if (titleRemoved) {
+  }
+}
+
+const dispatchPlayerProfile = (actionType, payload) => {
+  switch (actionType) {
+    case 'SET_PROFILE':
+      setPlayerProfile([ ...playerProfile, payload.newPlayer ]);
+      return;
+    case 'REMOVE_USER':
+      setPlayerProfile(playerProfile.filter(player => player.id !== payload.playerId));
+      return;
+    default:
+      return;
+  }
+};
 
   return (<>
-    <div className="App-background" >
-      <div className="App-container" >>
-        <div className='woodenBox' style={{height: height/2, width: width/2}}>
-          <SelfSimulator/>
+    <playerContext.Provider value={{playerProfile, dispatchPlayerProfile}}>
+      <div className="App-background">
+        <CommandLineTerminal/>
+        <div className="App-container" onClick={() => clickTitle()}>
+        <h1 className="App-title">ARCHETYPES</h1>
+          <SelfSimulator titleRemoved={titleRemoved}/>
         </div>
       </div>
-    </div>
+    </playerContext.Provider>
   </>
   );
 }
