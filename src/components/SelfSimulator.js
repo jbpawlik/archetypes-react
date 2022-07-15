5f5715f2b8b62e5463047320287b864f0804753c
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-14 18:16:01 -0700

    import css

--- src/components/SelfSimulator.js
+++ src/components/SelfSimulator.js
@@ -1,4 +1,4 @@
-import '../App.css';
+import '../css/App.css';
 import React, {useState, useEffect} from 'react'
 // import {useWindowDimensions} from '../functions/getWindowDimensions.js';
 import $ from 'jquery'
