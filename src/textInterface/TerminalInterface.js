3b405fbbc56722e936b578d151c94970dbae3362
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-15 03:43:57 -0700

    add askAnswer

--- src/textInterface/TerminalInterface.js
+++ src/textInterface/TerminalInterface.js
@@ -1,95 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { AttachAddon } from 'xterm-addon-attach';
-import xtermTheme from 'xterm-theme';
-import '../css/xterm.css'
-import 'chalkie'
-import archetypesText from '../objectLists/archtypesText.js';
+import { useEffect, useState } from 'react';
+// import xtermTheme from 'xterm-theme';
+import '../css/xterm.css';
+import 'chalkie';
+// import archetypesText from '../objectLists/archtypesText.js';
+// import ptyServer from '../components/ptyServer.js'
+import {openpty} from 'xterm-pty';
 
 export default function TerminalInterface() {
-  let term = new Terminal({
-    // rendererType: 'canvas',
-    rendererType: 'dom',
-    rows: 20,
-    cols: 84,
-    fontSize: 10,
-    fontFamily: 'Courier New',
-    fontWeightBold: 900,
-    cursorBlink: false,
-    cursorStyle: 'block',
-    theme: {
-      foreground:"#3b2322",
-      background:"#dfdbc3",
-      cursor:"#73635a",
-      black:"#000000",
-      brightBlack:"#808080",
-      red:"#cc0000",
-      brightRed:"#cc0000",
-      green:"#009600",
-      brightGreen:"#009600",
-      yellow:"#d06b00",
-      brightYellow:"#d06b00",
-      blue:"#0000cc",
-      brightBlue:"#0000cc",
-      magenta:"#cc00cc",
-      brightMagenta:"#cc00cc",
-      cyan:"#0087cc",
-      brightCyan:"#0087cc",
-      white:"#cccccc",
-      brightWhite:"#ffffff",
-    },
-    convertEol: true,
-    // restoreWin: true,
-    // setWinPosition: true,
-    // setWinSizePixels: true,
-    // screenReaderMode: true,
-    // windowsMode: true,
-  });
-  const socket = new WebSocket("ws://localhost:3000", "echo-protocol");
-  term.loadAddon(new AttachAddon(socket));
-  // const fitAddon = new FitAddon();
-  // term.loadAddon(fitAddon);
-  term.open(document.getElementById('terminal'));
-  // fitAddon.fit();
+  const { master, slave } = openpty();
+  const [playGame, setPlayGame] = useState(true)
+  const [playerInput, setPlayerInput] = useState('')
 
-  document.querySelector('.xterm').addEventListener('wheel', e => {
-    if (term.buffer.active.baseY > 0) {
-      e.preventDefault();
-    }
-  });
+  useEffect(() => {
 
-  function runFakeTerminal() {
-    if (term._initialized) {
-      return;
-    }
+    let term = new Terminal({
+      // rendererType: 'canvas',
+      rendererType: 'dom',
+      rows: 20,
+      cols: 84,
+      fontSize: 10,
+      fontFamily: 'Courier New',
+      fontWeightBold: 900,
+      cursorBlink: false,
+      cursorStyle: 'block',
+      tooMuchOutput: 'continue',
+      theme: {
+        foreground:"#3b2322",
+        background:"#dfdbc3",
+        cursor:"#73635a",
+        black:"#000000",
+        brightBlack:"#808080",
+        red:"#cc0000",
+        brightRed:"#cc0000",
+        green:"#009600",
+        brightGreen:"#009600",
+        yellow:"#d06b00",
+        brightYellow:"#d06b00",
+        blue:"#0000cc",
+        brightBlue:"#0000cc",
+        magenta:"#cc00cc",
+        brightMagenta:"#cc00cc",
+        cyan:"#0087cc",
+        brightCyan:"#0087cc",
+        white:"#cccccc",
+        brightWhite:"#ffffff",
+      },
+      convertEol: true,
+    });
 
-    term._initialized = true;
-
-    term.prompt = () => {
-      term.write('\r\n$ ');
-    };
-
-
-    // TODO: Use a nicer default font
-    term.write(`${chalk.ansi256(179)(`
-   █████╗ ██████╗  ██████╗██╗  ██╗███████╗████████╗██╗   ██╗██████╗ ███████╗███████╗
-  ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔════╝╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔════╝
-  ███████║██████╔╝██║     ███████║█████╗     ██║    ╚████╔╝ ██████╔╝█████╗  ███████╗
-  ██╔══██║██╔══██╗██║     ██╔══██║██╔══╝     ██║     ╚██╔╝  ██╔═══╝ ██╔══╝  ╚════██║
-  ██║  ██║██║  ██║╚██████╗██║  ██║███████╗   ██║      ██║   ██║     ███████╗███████║
-  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝      ╚═╝   ╚═╝     ╚══════╝╚══════╝
-    `)}`);
-
-    term.writeln('Below is a simple emulated backend, try running `help`.');
-    addDecoration(term);
-    prompt(term);
+    const socket = new WebSocket("ws://localhost:3000", "echo-protocol");
+    term.loadAddon(new AttachAddon(socket));
+    term.loadAddon(master)
 
     term.onData(e => {
       switch (e) {
         case '\u0003': // Ctrl+C
-          term.write('^C');
-          prompt(term);
+          // term.write('');
+          // prompt(term);
+          term.reset();
+          playArchetypes();
           break;
         case '\r': // Enter
           runCommand(term, command);
@@ -98,7 +72,7 @@ export default function TerminalInterface() {
         case '\u007F': // Backspace (DEL)
           // Do not delete the prompt
           if (term._core.buffer.x > 2) {
-            term.write('\b \b');
+            slave.write('\b \b');
             if (command.length > 0) {
               command = command.substr(0, command.length - 1);
             }
@@ -107,194 +81,173 @@ export default function TerminalInterface() {
         default: // Print all other characters for demo
           if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7E) || e >= '\u00a0') {
             command += e;
-            term.write(e);
+            // This line doubles the characters being typed in the console (but not the number of inputs)
+            // slave.write(e);
           }
       }
-    });
+    }
+    );
 
-    // Create a very simple link provider which hardcodes links for certain lines
-    term.registerLinkProvider({
-      provideLinks(bufferLineNumber, callback) {
-        // eslint-disable-next-line default-case
-        switch (bufferLineNumber) {
-          case 2:
-            callback([
-              {
-                text: 'VS Code',
-                range: { start: { x: 28, y: 2 }, end: { x: 34, y: 2 } },
-                activate() {
-                  window.open('https://github.com/microsoft/vscode', '_blank');
-                }
-              },
-              {
-                text: 'Hyper',
-                range: { start: { x: 37, y: 2 }, end: { x: 41, y: 2 } },
-                activate() {
-                  window.open('https://github.com/vercel/hyper', '_blank');
-                }
-              },
-              {
-                text: 'Theia',
-                range: { start: { x: 47, y: 2 }, end: { x: 51, y: 2 } },
-                activate() {
-                  window.open('https://github.com/eclipse-theia/theia', '_blank');
-                }
-              }
-            ]);
-            return;
-          case 8:
-            callback([
-              {
-                text: 'WebGL renderer',
-                range: { start: { x: 54, y: 8 }, end: { x: 67, y: 8 } },
-                activate() {
-                  window.open('https://npmjs.com/package/xterm-addon-webgl', '_blank');
-                }
-              }
-            ]);
-            return;
-          case 14:
-            callback([
-              {
-                text: 'Links',
-                range: { start: { x: 45, y: 14 }, end: { x: 49, y: 14 } },
-                activate() {
-                  window.alert('You can handle links any way you want');
-                }
-              },
-              {
-                text: 'themes',
-                range: { start: { x: 52, y: 14 }, end: { x: 57, y: 14 } },
-                activate() {
-                  isBaseTheme = !isBaseTheme;
-                  term.setOption('theme', isBaseTheme ? baseTheme : otherTheme);
-                  document.querySelector('.demo .inner').classList.toggle('other-theme', !isBaseTheme);
-                  term.write(`\r\nActivated ${isBaseTheme ? 'xterm.js' : 'snazzy'} theme`);
-                  prompt(term);
-                }
-              },
-              {
-                text: 'addons',
-                range: { start: { x: 60, y: 14 }, end: { x: 65, y: 14 } },
-                activate() {
-                  window.open('/docs/guides/using-addons/', '_blank');
-                }
-              }
-            ]);
-            return;
-          case 15: callback([
-            {
-              text: 'typed API',
-              range: { start: { x: 45, y: 15 }, end: { x: 53, y: 15 } },
-              activate() {
-                window.open('https://github.com/xtermjs/xterm.js/blob/master/typings/xterm.d.ts', '_blank');
-              }
-            },
-            {
-              text: 'decorations',
-              range: { start: { x: 56, y: 15 }, end: { x: 66, y: 15 } },
-              activate() {
-                window.open('https://github.com/xtermjs/xterm.js/blob/master/typings/xterm.d.ts#L947', '_blank');
-              }
-            },
-          ]);
-            return;
-        }
-        callback(undefined);
+    let command = '';
+    let commands = {
+      help: {
+        f: () => {
+          term.writeln([
+            'Welcome to xterm.js! Try some of the commands below.',
+            '',
+            ...Object.keys(commands).map(e => `  ${e.padEnd(10)} ${commands[e].description}`)
+          ].join('\n\r'));
+          prompt(term);
+        },
+        description: 'Prints this help message',
+      },
+      ls: {
+        f: () => {
+          term.writeln(['a', 'bunch', 'of', 'fake', 'files'].join('\r\n'));
+          term.prompt(term);
+        },
+        description: 'Prints a fake directory structure'
+      },
+      loadtest: {
+        f: () => {
+          let testData = [];
+          let byteCount = 0;
+          for (let i = 0; i < 50; i++) {
+            let count = 1 + Math.floor(Math.random() * 79);
+            byteCount += count + 2;
+            let data = new Uint8Array(count + 2);
+            data[0] = 0x0A; // \n
+            for (let i = 1; i < count + 1; i++) {
+              data[i] = 0x61 + Math.floor(Math.random() * (0x7A - 0x61));
+            }
+            // End each line with \r so the cursor remains constant, this is what ls/tree do and improves
+            // performance significantly due to the cursor DOM element not needing to change
+            data[data.length - 1] = 0x0D; // \r
+            testData.push(data);
+          }
+          let start = performance.now();
+          for (let i = 0; i < 1024; i++) {
+            for (const d of testData) {
+              term.write(d);
+            }
+          }
+          // Wait for all data to be parsed before evaluating time
+          term.write('', () => {
+            let time = Math.round(performance.now() - start);
+            let mbs = ((byteCount / 1024) * (1 / (time / 1000))).toFixed(2);
+            term.write(`\n\r\nWrote ${byteCount}kB in ${time}ms (${mbs}MB/s) using the ${isWebglEnabled ? 'webgl' : 'canvas'} renderer`);
+            term.prompt();
+          });
+        },
+        description: 'Simulate a lot of data coming from a process'
       }
-    });
-  }
+  };
 
   function prompt(term) {
     command = '';
-    term.write('\r\n$ ');
+    term.write('\r\n ');
   }
-
-  let command = '';
-  let commands = {
-    help: {
-      f: () => {
-        term.writeln([
-          'Welcome to xterm.js! Try some of the commands below.',
-          '',
-          ...Object.keys(commands).map(e => `  ${e.padEnd(10)} ${commands[e].description}`)
-        ].join('\n\r'));
-        prompt(term);
-      },
-      description: 'Prints this help message',
-    },
-    ls: {
-      f: () => {
-        term.writeln(['a', 'bunch', 'of', 'fake', 'files'].join('\r\n'));
-        term.prompt(term);
-      },
-      description: 'Prints a fake directory structure'
-    },
-    loadtest: {
-      f: () => {
-        let testData = [];
-        let byteCount = 0;
-        for (let i = 0; i < 50; i++) {
-          let count = 1 + Math.floor(Math.random() * 79);
-          byteCount += count + 2;
-          let data = new Uint8Array(count + 2);
-          data[0] = 0x0A; // \n
-          for (let i = 1; i < count + 1; i++) {
-            data[i] = 0x61 + Math.floor(Math.random() * (0x7A - 0x61));
-          }
-          // End each line with \r so the cursor remains constant, this is what ls/tree do and improves
-          // performance significantly due to the cursor DOM element not needing to change
-          data[data.length - 1] = 0x0D; // \r
-          testData.push(data);
-        }
-        let start = performance.now();
-        for (let i = 0; i < 1024; i++) {
-          for (const d of testData) {
-            term.write(d);
-          }
-        }
-        // Wait for all data to be parsed before evaluating time
-        term.write('', () => {
-          let time = Math.round(performance.now() - start);
-          let mbs = ((byteCount / 1024) * (1 / (time / 1000))).toFixed(2);
-          term.write(`\n\r\nWrote ${byteCount}kB in ${time}ms (${mbs}MB/s) using the ${isWebglEnabled ? 'webgl' : 'canvas'} renderer`);
-          term.prompt();
-        });
-      },
-      description: 'Simulate a lot of data coming from a process'
-    }
-  };
 
   function runCommand(term, text) {
     const command = text.trim().split(' ')[0];
     if (command.length > 0) {
-      term.writeln('');
+      // term.writeln('');
       if (command in commands) {
         commands[command].f();
         return;
       }
-      term.writeln(`${command}: command not found`);
+      // term.writeln(`${command}: command not found`);
     }
     prompt(term);
   }
 
-function addDecoration(term) {
-  const marker = term.addMarker(15);
-  const decoration = term.registerDecoration({ marker, x: 44 });
-  decoration.onRender(element => {
-    element.classList.add('link-hint-decoration');
-    // must be inlined to override inlined width/height coming from xterm
-    element.style.height = '';
-    element.style.width = '';
-  });
-}
+    function playArchetypes() {
+      let userInput = '';
 
-  // let playGame = true
+      if (slave._initialized) {
+        return;
+      }
+  
+      slave._initialized = true;
+  
+      slave.prompt = () => {
+        slave.write('\r\n');
+      };
+  
+      slave.write(`${chalk.ansi256(179)(`
+   █████╗ ██████╗  ██████╗██╗  ██╗███████╗████████╗██╗   ██╗██████╗ ███████╗███████╗
+  ██╔══██╗██╔══██╗██╔════╝██║  ██║██╔════╝╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔════╝
+  ███████║██████╔╝██║     ███████║█████╗     ██║    ╚████╔╝ ██████╔╝█████╗  ███████╗
+  ██╔══██║██╔══██╗██║     ██╔══██║██╔══╝     ██║     ╚██╔╝  ██╔═══╝ ██╔══╝  ╚════██║
+  ██║  ██║██║  ██║╚██████╗██║  ██║███████╗   ██║      ██║   ██║     ███████╗███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝      ╚═╝   ╚═╝     ╚══════╝╚══════╝
+      \n`)}`);
+
+      function askAnswer(text) {
+        let textArray = [];
+        slave.write(text)
+        slave.prompt()
+        slave.onReadable(() => {
+          const textInput = slave.read()
+          console.log(textInput)
+          textInput.forEach(number => {
+            textArray.push(String.fromCharCode(number))
+          })
+          console.log(textArray)
+          userInput = textInput
+          slave.write(`Hi, ${ textArray.join('') }!\n`)
+          // setPlayerInput(textInput)
+          // console.log(playerInput)
+          // slave.write(playerInput)
+          // userInput = String.fromCharCode(97,100,119,97,100,10)
+          console.log(userInput)
+        })
+        // console.log(userInput)
+        // return userInput
+        // console.log(playerInput)
+      }
+
+      askAnswer('Input your name: ')
+      // slave.write("Input your name:");
+      // slave.onReadable(() => {
+      //   // slave.write(slave.read())
+      //   const text = slave.read().toString()
+      //   console.log(text)
+      //   slave.write(text);
+      // });
+      // console.log(slave.read());
+      // slave.prompt()
+      // console.log(slave.read());
+      // slave.onReadable(() => {
+      //   // slave.write(slave.read())
+      //   const text = slave.read().toString()
+      //   console.log(text)
+      //   slave.write(text);
+      // });
+    }
+
+    term.open(document.getElementById('terminal'));
+    // const fitAddon = new FitAddon();
+    // term.loadAddon(fitAddon);
+    // fitAddon.fit();
 
 
-  // while (playGame) {
+    document.querySelector('.xterm').addEventListener('wheel', e => {
+      if (term.buffer.active.baseY > 0) {
+        e.preventDefault();
+      }
+    });
 
-    runFakeTerminal();
+    playArchetypes();
+    return () => {
+      setPlayGame(false)
+    }
+  }, [playGame])
+
+
+  if (playGame) {
+
+    // runFakeTerminal();
 
   // let curr_line = "";
   // let entries = [];
@@ -306,7 +259,7 @@ function addDecoration(term) {
 //   term.onResize(function (evt) {
 //     socket.send({ rows: evt.rows });
 //  });
-  
+
 //  term.onKey(e => {
 //     curr_line += e.key;
 //     term.write(e.key);
@@ -358,6 +311,17 @@ function addDecoration(term) {
     // term.write(archetypesText[0])
   //   term.prompt();
 
+
+  // slave.onReadable((text)=> {
+  //   console.log(text)
+  //   if (slave.read() === 'Yes' ||  slave.read() === 'Y') {
+  //     // term.reset()
+  //     // playGame=true
+  //   } else {
+  //     // playGame = false
+  //   }
+  //   term.write(slave.read())
+  // })
   //   const playerName = rs.question("\nWhat is your name?\n\n");
 
   //   console.log(chalk.green(`\nWelcome ${playerName}` + `, answer these questions to receive an archetype\n`));
@@ -520,4 +484,4 @@ function addDecoration(term) {
   //     break
   //   }
   }
-// }
+}
