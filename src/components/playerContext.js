89249c9c6d2457a156c19448eb588ce71ede4c2d
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-20 07:57:55 -0700

    update playerContext

--- src/components/playerContext.js
+++ src/components/playerContext.js
@@ -1,27 +1,3 @@
 import { createContext } from "react";
 
-export const playerContext = createContext();
-
-// import React, { createContext, useState } from "react";
-// import { playerProfile } from "./playerProfile.js";
-
-// const PlayerContext = createContext(playerProfile);
-// const PlayerDispatchContext = createContext(undefined);
-
-// function PlayerProvider({ children }) {
-//   const [playerDetails, setPlayerDetails] = useState({
-//     playerAttributes: [],
-//     playerArchetype: [],
-//     playerOpposite: []
-//   });
-
-//   return (
-//     <PlayerContext.Provider value={playerDetails}>
-//       <PlayerDispatchContext.Provider value={setPlayerDetails}>
-//         {children}
-//       </PlayerDispatchContext.Provider>
-//     </PlayerContext.Provider>
-//   );
-// }
-
-// export { PlayerProvider, PlayerContext, PlayerDispatchContext };
+export const PlayerContext = createContext();
