df692241ae3ff9aee3a846295806602e4eaf6c5c
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-07-20 07:58:59 -0700

    Context in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './css/App.css';
-import React, {useState } from 'react'
+import React, {useState} from 'react'
 import SelfSimulator from './components/SelfSimulator.js'
 import $ from 'jquery'
 import useSound from 'use-sound'
@@ -8,16 +8,33 @@ import blanket from './assets/sounds/blanket.wav';
 import sweatpants from './assets/sounds/sweatpants.wav';
 import woodclack from './assets/sounds/woodclack.wav'
 import woodtap from './assets/sounds/woodtap.wav';
-import { playerContext } from './components/playerContext.js';
+import { PlayerContext } from './components/PlayerContext.js';
 import CommandLineTerminal from './components/CommandLineTerminal.js';
+import AddPlayer from './components/AddPlayer.js';
+import PlayerList from './components/PlayerList.js';
+
 // import {TextInterface} from './TextInterface/TextInterface.js';
 // import {useWindowDimensions} from './functions/getWindowDimensions.js';
 
 function App() {
   // const { height, width } = useWindowDimensions();
   const [intro] = useSound(woodclack)
-  const [titleRemoved, removeTitle] = useState(false)
-  const [playerProfile, setPlayerProfile] = useState({}) 
+  const [titleRemoved, removeTitle] = useState(false);
+  const [playerProfile, setPlayerProfile] = useState([{id: 1, name: 'hi', age: 4, bio: 'hazard'}])
+
+  const dispatchPlayerProfile = (actionType, payload) => {
+    switch (actionType) {
+      case 'UPDATE_PLAYER':
+        setPlayerProfile([ ...playerProfile, payload.newPlayer ]);
+        return;
+      case 'RESET_PLAYER':
+        setPlayerProfile(playerProfile.filter(player => player.id !== payload.id));
+        return;
+      default:
+        return;
+    }
+  };
+
 
   const clickTitle = () => {
     if (!titleRemoved) {
@@ -25,34 +42,25 @@ function App() {
     $('.App-title').remove()
     removeTitle(true)
     $('.main-console').css('display', 'flex')
-  } else if (titleRemoved) {
+    } else if (titleRemoved) {
   }
-}
+  }
 
-const dispatchPlayerProfile = (actionType, payload) => {
-  switch (actionType) {
-    case 'SET_PROFILE':
-      setPlayerProfile([ ...playerProfile, payload.newPlayer ]);
-      return;
-    case 'REMOVE_USER':
-      setPlayerProfile(playerProfile.filter(player => player.id !== payload.playerId));
-      return;
-    default:
-      return;
-  }
-};
 
   return (<>
-    <playerContext.Provider value={{playerProfile, dispatchPlayerProfile}}>
+    <PlayerContext.Provider value={{ playerProfile, dispatchPlayerProfile }}>
+      <AddPlayer></AddPlayer>
+      <PlayerList></PlayerList>
       <div className="App-background">
-        <CommandLineTerminal />
+      <CommandLineTerminal />
+
 
         {/* <div className="App-container" onClick={() => clickTitle()}>
         <h1 className="App-title">ARCHETYPES</h1>
           <SelfSimulator titleRemoved={titleRemoved}/>
         </div> */}
-     </div>
-    </playerContext.Provider>
+      </div>
+    </PlayerContext.Provider>
   </>
   );
 }
